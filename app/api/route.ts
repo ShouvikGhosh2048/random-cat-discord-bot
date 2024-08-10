afb086edb18a9cf738e17bc8ca5bce0483b244b7
Shouvik Ghosh <shouvikg.cal@gmail.com>
2024-08-10 10:34:08 +0530

    Send image to discord

---
+++ app/api/route.ts
@@ -0,0 +1,19 @@
+export const dynamic = 'force-dynamic';
+
+export async function GET(request: Request) {
+    const response = await fetch("https://cataas.com/cat?json=true");
+    const { _id } = await response.json();
+
+    // https://www.youtube.com/watch?v=zdV_fXjoB-Q
+    await fetch(process.env.WEBHOOK_URL!, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            content: `https://cataas.com/cat/${_id}`
+        })
+    });
+
+    return new Response(`${_id}`);
+}
