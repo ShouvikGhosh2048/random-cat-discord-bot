a0df087d331004cbb257b5c8f329b15095bb3b7b
Shouvik Ghosh <shouvikg.cal@gmail.com>
2025-02-22 19:24:33 +0530

    Update field

--- app/api/route.ts
+++ app/api/route.ts
@@ -5,7 +5,7 @@ export async function GET(request: Request) {
         "https://cataas.com/cat?json=true",
         { cache: "no-store" }
     );
-    const { _id } = await response.json();
+    const { id } = await response.json();
 
     // https://www.youtube.com/watch?v=zdV_fXjoB-Q
     await fetch(process.env.WEBHOOK_URL!, {
